85b30a0f4e73ce0dc6a6607e9a55da4a523553bf
KablammoMan <codernaffilio@gmail.com>
2023-03-15 16:04:41 +1000

    Update main.py

--- main.py
+++ main.py
@@ -18,10 +18,11 @@ ver = get_content("https://github.com/KablammoMan/SelfUpdater/raw/main/version.j
 while not ver:
     ver = get_content("https://github.com/KablammoMan/SelfUpdater/raw/main/version.json")
 
-latest_version = ver.decode()
+latest_version = ver.decode()["latest_version"]
 
 if latest_version > THIS_VERSION:
     newc = get_content("https://github.com/KablammoMan/SelfUpdater/raw/main/client.py")
     while not newc:
         newc = get_content("https://github.com/KablammoMan/SelfUpdater/raw/main/client.py")
-        
+
+print(newc.decode)
